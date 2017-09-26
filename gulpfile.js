5cc0c4c4e4a9331f8e523cb8e8f97ada16e34d1f
Elias Soares <elias_soares95@hotmail.com>
2017-09-25 21:22:18 -0300

    Ajuste gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
-var gulp = require('gulp'),
-watch = require('gulp-watch'),
-postcss = require('gulp-postcss'),
-cssImport = require('postcss-import'),
-browserSync = require('browser-sync').create();
+const gulp = require('gulp');
+const watch = require('gulp-watch');
+const postcss = require('gulp-postcss');
+const cssImport = require('postcss-import');
+const browserSync = require('browser-sync').create();
 
 gulp.task('watch', function(){
     browserSync.init({
@@ -16,18 +16,18 @@ gulp.task('watch', function(){
         browserSync.reload();
     });
 
-    watch('./app/assets/**/*.css', function(){
+    watch('./app/src/**/*.css', function(){
         gulp.start('cssInject');
     });
 })
 
 gulp.task('cssInject', ['styles'], function(){
-    return gulp.src('./app/dist/main.css')
+    return gulp.src('./app/dist/css/main.css')
         .pipe(browserSync.stream());
 });
 
 gulp.task('styles', function(){
-    return gulp.src('./app/assets/css/main.css')
+    return gulp.src('./app/src/css/main.css')
         .pipe(postcss([cssImport]))
-        .pipe(gulp.dest('./app/dist'));
+        .pipe(gulp.dest('./app/dist/css'));
 });
